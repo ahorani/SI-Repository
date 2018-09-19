3535f526896e68efe7714649d270dc09a13ca536
ahorani <42815593+ahorani@users.noreply.github.com>
2018-09-19 15:10:12 -0700

    Add files via upload

---
+++ practice9_19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  std::string _x = "one";
+  std::string y = "two";
+  std::string z = "three";
+  std::cout<<_x <<"\n" <<y <<"\n" <<z <<"\n";
+
+  return 0;
+}
---
+++ program1_9_19.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+/*int main()
+{
+  std::string name = "Azeem";
+  int year = 2022;
+  std::string major = "Computer Engineering";
+
+  std::cout<<name <<"\n" <<year <<"\n" <<major <<"\n";
+
+  return 0;
+}*/
+int main()
+{
+  int x = 25;
+  int y = 75;
+  int z = x+y;
+
+  std::cout<<x <<"+" <<y <<"=" <<z <<"\n";
+
+  return 0;
+}
